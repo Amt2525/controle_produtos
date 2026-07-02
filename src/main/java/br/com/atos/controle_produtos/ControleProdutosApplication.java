7ec06c635cd9890da6861071c6cb49640eda0001
Atos_Ads <amt3@aluno.ifnmg.edu.br>
2026-07-01 21:33:42 -0300

    Primeiro commit

---
+++ src/main/java/br/com/atos/controle_produtos/ControleProdutosApplication.java
@@ -0,0 +1,13 @@
+package br.com.atos.controle_produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleProdutosApplication.class, args);
+	}
+
+}
